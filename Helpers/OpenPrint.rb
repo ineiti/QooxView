687b15794ed18d8107f5a1b0f406d3e84ac3b4da
Linus Gasser <ineiti@linusetviviane.ch>
2012-12-12 11:22:56 +0100

    Added a good split-button for printers with cups-detection

--- Helpers/OpenPrint.rb
+++ Helpers/OpenPrint.rb
@@ -58,3 +58,70 @@ class OpenPrint
     end
   end
 end
+
+module PrintButton
+  def get_remote_printers(ip)
+    ddputs(4){"Getting printers for #{ip}"}
+    %x( lpstat -h #{ip}:631 -a | sed -e "s/ .*//" ).split
+  end
+  
+  def get_local_printers
+    get_remote_printers("localhost")
+  end
+  
+  def show_print( *buttons )
+    ddputs(3){"show_print with #{buttons.inspect}"}
+    if not instance_variable_defined? :@default_printer
+      @default_printer = {}
+    end
+    print_name = nil
+    gui_hbox :nogroup do
+      buttons.to_a.each{|b|
+        ddputs(4){"Doing #{b.inspect}"}
+        if b.to_s =~ /^print/
+          show_split_button b, get_local_printers
+          print_name = b.to_sym
+        else
+          show_button b
+        end
+      }
+      if not print_name
+        show_split_button :print, get_local_printers
+        print_name = :print
+      end
+      @default_printer[print_name] = 
+        Entities.Statics.get(self.name.to_s + print_name.to_s)
+      if @default_printer[print_name].data_str == ""
+        @default_printer[print_name].data_str = get_local_printers.first
+      end
+    end
+  end
+  
+  def reply_print(session)
+    ret = []
+    @default_printer.each{|k,v|
+      ddputs(4){"#{k}-#{v.inspect}"}
+      value = "#{GetText._( k.to_s )} #{v.data_str}"
+      if ip = session.web_req.peeraddr[3]
+        if not ip =~ /(::1|localhost|127.0.0.1)/
+          value = [ value ] + get_local_printers + get_remote_printers(ip)
+        end
+      end
+      ret += reply( :update, k => value )
+    }
+    ddputs(4){"#{ret.inspect}"}
+    ret
+  end
+  
+  def get_printer( name )
+    @default_printer[name.to_sym].data_str
+  end
+  
+  def rpc_print( session, name, data )
+    ddputs(4){"Printing button #{name} with #{data.inspect}"}
+    if data['menu'] and data['menu'].length > 0
+      @default_printer[name.to_sym].data_str = data['menu']
+    end
+    reply_print( session )
+  end
+end
