6b15cf95f205c52f7fa3f160a2c844588c511b20
Linus Gasser <ineiti@linusetviviane.ch>
2013-09-20 11:56:54 +0100

    added files

---
+++ Example/1-7-LongList/Start.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby -I../..
+
+require 'QooxView'
+
+DEBUG_LVL=2
+
+Welcome.nologin
+
+class PersonEdit < View
+  def layout
+    gui_vbox do
+      show_list :hi, :flexheight => 1
+    end
+    @update = true
+  end
+
+  def rpc_update( session )
+    reply( :update, :hi => (1..30).collect{|a| [a,a] } )
+  end
+end
+
+QooxView::startWeb
+
