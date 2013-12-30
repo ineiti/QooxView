a04147dd17765394b3eb82af3e9be556c33bcbf1
Linus Gasser <ineiti@linusetviviane.ch>
2013-12-30 11:46:01 +0100

    Added files for git

---
+++ Example/2-1-Display-small/Start.rb
@@ -0,0 +1,34 @@
+#!/usr/local/bin/ruby -I../..
+
+require 'QooxView'
+
+DEBUG_LVL=2
+
+Welcome.nologin
+
+
+class ResizeTabs < View
+  def layout
+    @order = 0
+    gui_vbox :nogroup do
+        show_str :search
+    end
+    @update = true
+  end
+end
+
+class ResizesTabs < View
+  def layout
+  end
+end
+
+class ResizeIt < View
+  def layout
+    gui_vbox :nogroup do
+      show_button :mylist, :yourlist
+    end
+  end
+end
+
+QooxView::startWeb
+
