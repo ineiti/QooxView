31866a984a94d6244718d2b45931b150468e3570
Linus Gasser <ineiti@linusetviviane.ch>
2015-04-23 16:39:02 +0100

    worked on login-screen

--- Helpers/Welcome.rb
+++ Helpers/Welcome.rb
@@ -1,6 +1,6 @@
 class Welcome < View
   attr_accessor :no_login
-  
+
   def layout
     gui_vbox :nogroup do
       gui_hbox :nogroup do
@@ -9,15 +9,11 @@ class Welcome < View
         show_str :username
         show_pass :password
         show_str_ro :version
-        if get_config( false, :Views, :Welcome, :direct_internet )
-          show_button :login, :direct_connect
-        else
-          show_button :login
-        end
+        show_button :simple_connect, :direct_connect
         gui_fields :nogroup do
         end
       end
-      
+
       gui_window :login_failed do
         show_html :reason
         show_button :try_again
@@ -28,32 +24,32 @@ class Welcome < View
         end
       end
     end
-        
-    dputs( 5 ){ "#{@layout.inspect}" }
+
+    dputs(5) { "#{@layout.inspect}" }
     @visible = false
     @no_login ||= false
   end
-  
-  def rpc_show( session )
-    dputs( 3 ){ self.inspect }
+
+  def rpc_show(session)
+    dputs(3) { self.inspect }
     if @no_login
-      dputs( 2 ){ 'No login is enabled' }
+      dputs(2) { 'No login is enabled' }
       session
-      return reply( :session_id, session.sid ) + 
-        View.rpc_list( session )
+      return reply(:session_id, session.sid) +
+          View.rpc_list(session)
     else
-      dputs( 2 ){ 'Login is enabled' }
-      super( session )
+      dputs(2) { 'Login is enabled' }
+      super(session) + reply(:hide, :direct_connect)
     end
   end
-  
+
   def self.nologin
-    dputs( 2 ){ 'Going for no login' }
+    dputs(2) { 'Going for no login' }
     $config.__Views.__Welcome.__no_login = true
     View.Welcome.no_login = true
   end
-  
-  def rpc_button_try_again( session, data )
-    reply( :window_hide )
+
+  def rpc_button_try_again(session, data)
+    reply(:window_hide)
   end
 end
