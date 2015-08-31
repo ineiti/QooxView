28016347b1e0f8bfd6238dbc98fcfb2ed48267bc
Linus Gasser <ineiti@linusetviviane.ch>
2015-08-31 07:33:31 +0200

    Undebugging 'dp'

--- lib/qooxview/entity.rb
+++ lib/qooxview/entity.rb
@@ -605,19 +605,6 @@ class Entity
     field = field_orig.to_s
     (direct = field =~ /^_/) and field.sub!(/^_/, '')
     dputs(4) { "Direct is #{direct.inspect} for field #{field_orig.inspect}" }
-=begin
-    v = if value.is_a? Entity
-      dputs( 3 ){ "Converting #{value} to #{value.id}" }
-      value.id
-    elsif value.is_a? Array
-      dputs(3){"Storing an array #{value.inspect}"}
-      dp value.collect{|val|
-        val.is_a?( Entity ) ? val.id : val
-      }
-    else
-      value
-    end
-=end
     v = value
     dputs(4) { "Self is #{self.public_methods.sort.inspect}" }
     if (self.public_methods.index("#{field}=".to_sym)) && (not direct)
